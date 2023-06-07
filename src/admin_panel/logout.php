162f9e29bfa77ec1402fa7267736605f63fc0a2a
Godlove <akalegodlove@gmail.com>
2023-06-07 15:19:23 +0100

    clean it

--- src/admin_panel/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-  // database connection file
-include '../config/config.inc.php';
-    // session_start();
-	session_destroy();
-    //2. REdirect to Login Page
-    header('location:../');
-    exit();
-?> 
--- src/admin_panel/partials/footer.php
+++
@@ -1,9 +0,0 @@
-<!-- footer -->
-<footer class="static bottom-0 text-xs bg-gray-200 p-2 mt-4">
-<p class="text-center text-gray-500">&copy; 2023 All rights Reserved,Anonymous.</p>
-</footer>
-
-<script src="../assets/js/admin-menu.js"></script>
-<script src="../assets/js/imgpreview.js"></script>
-</body>
-</html>
